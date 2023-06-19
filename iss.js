fafd40f546fc47e0408c19db3249c049745172fa
Afreeda <amahesan@uoguelph.ca>
2023-06-19 08:57:09 -0400

    lint code

--- iss.js
+++ iss.js
@@ -9,7 +9,7 @@
 
 const request = require('request');
 
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
       callback(error, null);
@@ -24,7 +24,7 @@ const fetchMyIP = function(callback) {
       const ip = (JSON.parse(body)).ip;
       callback(null, ip);
     }
-  })
-}
+  });
+};
 
 module.exports = { fetchMyIP };
